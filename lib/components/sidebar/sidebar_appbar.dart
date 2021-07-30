9c2a44ef4bf041fd705bf200c47e5aa324a86927
Andrew Ellen <18004410@uhi.ac.uk>
2021-07-31 00:43:35 +0100

    Auto stash before checking out "HEAD"

--- lib/components/sidebar/sidebar_appbar.dart
+++ lib/components/sidebar/sidebar_appbar.dart
@@ -8,25 +8,39 @@ class SideAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: RichText(
-        text: TextSpan(
-          children: <TextSpan>[
-            TextSpan(
-              text: "My",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                color: Color.fromRGBO(2, 7, 1, 1.0),
+      title: Center(
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 47),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, '/Home');
+                },
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "My",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            color: Color.fromRGBO(2, 7, 1, 1.0),
+                          ),
+                        ),
+                        TextSpan(
+                          text: "Terra",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            color: Color.fromRGBO(77, 227, 55, 1.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            ),
-            TextSpan(
-              text: "Terra",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                color: Color.fromRGBO(77, 227, 55, 1.0),
-              ),
-            ),
           ],
         ),
       ),
