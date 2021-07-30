9e30c2af555be0170451e2cdc3e22f66b5015973
Andrew Ellen <18004410@uhi.ac.uk>
2021-07-30 22:18:32 +0100

    Cards fully working from API

--- lib/components/product_card/card_titles.dart
+++ lib/components/product_card/card_titles.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:mobile_app/utilities/index.dart';
 
 class CardTitles extends StatelessWidget {
+  CardTitles({required this.title, required this.description});
+  final String title, description;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          'Product Name',
+          title,
           style: cardHeaderStyle,
         ),
         SizedBox(height: 2),
         Text(
-          'Company/ Org',
+          description,
           style: cardSubHeaderStyle,
         ),
         SizedBox(height: 2)
