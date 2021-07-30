db68f617243601c3f748b4507e3819a4c2d9a1a1
Andrew Ellen <18004410@uhi.ac.uk>
2021-07-30 16:11:55 +0100

    displaying from API not very well

--- lib/utilities/data/products.dart
+++ lib/utilities/data/products.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:mobile_app/models/product.dart';
 import 'package:http/http.dart' as http;
 
 Future<Product> getProducts() async {
@@ -9,12 +8,36 @@ Future<Product> getProducts() async {
   });
 
   if (response.statusCode == 200) {
-    //print(response.body);
-    var productlist = jsonDecode(response.body[0]);
-    assert(productlist is List);
-    print(productlist);
-    return Product.fromJson(productlist);
+    return Product.fromJson(json.decode(response.body));
   } else {
     throw Exception('Failed to load');
+  }
+}
+
+class Product {
+  final String productName;
+  final String productDescription;
+  final String imgName;
+  final double stars;
+  final String category;
+
+  Product({
+    required this.productName,
+    required this.productDescription,
+    required this.imgName,
+    required this.stars,
+    required this.category
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    print("results:");
+    print(json['entries'][0]['imgName']);
+    return Product(
+      productName: json['entries'][0]['productName'],
+      productDescription: json['entries'][0]['productDescription'],
+      imgName: json['entries'][0]['imgName'],
+      stars: json['entries'][0]['stars'],
+      category: json['entries'][0]['category'],
+    );
   }
 }
