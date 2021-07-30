56ba65b99d38351137fe16066b97a958486ba3ac
Andrew Ellen <18004410@uhi.ac.uk>
2021-07-30 02:40:10 +0100

    API call works. JSON not parsing properly

--- lib/utilities/data/products.dart
+++ lib/utilities/data/products.dart
@@ -4,11 +4,16 @@ import 'package:mobile_app/models/product.dart';
 import 'package:http/http.dart' as http;
 
 Future<Product> getProducts() async {
-  final response = await http
-      .get(Uri.parse('https://f758ae82.eu-gb.apigw.appdomain.cloud/productslist'));
+  final response = await http.get(Uri.parse('https://f758ae82.eu-gb.apigw.appdomain.cloud/productslist/products'), headers: {
+    'accept': "application/json",
+  });
 
   if (response.statusCode == 200) {
-    return Product.fromJson(jsonDecode(response.body));
+    //print(response.body);
+    var productlist = jsonDecode(response.body[0]);
+    assert(productlist is List);
+    print(productlist);
+    return Product.fromJson(productlist);
   } else {
     throw Exception('Failed to load');
   }
